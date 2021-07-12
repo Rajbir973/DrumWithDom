90451915c4f7b03ab2a0c6ef06b0eedd335337e3
Rajbir Singh <rajbirs973@gmail.com>
2021-07-12 14:26:52 -0400

    this file does exactly same things but using Jquery

---
+++ index2.js
@@ -0,0 +1,82 @@
+
+var numberOfButtons= $(".drum").length
+
+
+for(var i=0;i<numberOfButtons;i++)
+{
+    
+    $(".drum").click(function()
+    {
+        var btnHtml=$(this).html()
+
+        makeSound(btnHtml)
+
+        addAnimation(btnHtml)
+    })
+  
+}
+   $("body").keydown(function(event)
+   {
+       makeSound(event.key)
+       addAnimation(event.key)
+
+   })
+
+
+
+function makeSound(keys)
+{
+  switch (keys) {
+    case "w":
+      var audio1=new Audio("sounds/tom-1.mp3")
+      audio1.play()
+    break;
+
+    case "a":
+      var audio2=new Audio("sounds/tom-2.mp3")
+      audio2.play()
+    break;
+
+    case "s":
+      var audio3=new Audio("sounds/tom-3.mp3")
+      audio3.play()
+    break;
+
+    case "d":
+      var audio4=new Audio("sounds/tom-4.mp3")
+      audio4.play()
+    break;
+
+    case"j":
+       var audio5=new Audio("sounds/snare.mp3")
+       audio5.play()
+    break;
+    
+    case"k":
+       var audio6=new Audio("sounds/crash.mp3")
+       audio6.play()
+    break;
+    
+    case"l":
+      var audio7=new Audio("sounds/kick-bass.mp3")
+      audio7.play() 
+    break;
+
+    default:
+      break;
+  }
+
+}
+
+function addAnimation(keyactive)
+{
+
+ var activeBtn=$("."+keyactive)
+ activeBtn.addClass("pressed-button")
+
+ setTimeout(function(){
+   activeBtn.removeClass("pressed-button")
+ },100)
+
+
+}
